2976d861fa70725081b054d053d2ee7644ae2e54
Dane Murphy <danem7@gmail.com>
2021-12-30 19:34:59 -0800

    improved logging in payments service and config file added

--- payments/cmd/payments.go
+++ payments/cmd/payments.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net"
 
+	"github.com/hashicorp/hcl/v2/hclsimple"
+	"github.com/jessevdk/go-flags"
 	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 
@@ -13,10 +15,22 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-type opts struct {
-	Debug bool  `short:"d" long:"debug" description:"set logs to debug level"`
-	Port  int32 `short:"p" long:"port" description:"the port to run the gRPC server"`
-}
+const (
+	defaultConfigPath = "/app/configs/payments.hcl"
+)
+
+type (
+	opts struct {
+		Debug      bool   `short:"d" long:"debug" description:"set logs to debug level"`
+		Port       int32  `short:"p" long:"port" description:"the port to run the gRPC server"`
+		ConfigFile string `short:"f" long:"file" description:"the path to the configuration file"`
+	}
+
+	Config struct {
+		Debug bool  `hcl:"debug" json:"debug"`
+		Port  int32 `hcl:"port" json:"port"`
+	}
+)
 
 func main() {
 	var (
@@ -27,31 +41,66 @@ func main() {
 		srv        *service.Service
 		logger     *logrus.Logger
 		formatter  *logrus.JSONFormatter
+		cfg        Config
+		port       int32
+		debug      bool
+		cfgPath    string
 		address    string
 		err        error
 	)
 
+	if _, err = flags.Parse(&cmdOpts); err != nil {
+		panic(err)
+	}
+
 	logger = logrus.New()
 	formatter = new(logrus.JSONFormatter)
+
+	cfgPath = defaultConfigPath
+	if cmdOpts.ConfigFile != "" {
+		cfgPath = cmdOpts.ConfigFile
+	}
+
+	if err = hclsimple.DecodeFile(cfgPath, nil, &cfg); err != nil {
+		panic(err)
+	}
+
+	debug = cfg.Debug
 	if cmdOpts.Debug {
+		debug = cmdOpts.Debug
+	}
+
+	if debug {
 		formatter.PrettyPrint = true
 		logger.Level = logrus.DebugLevel
 	}
 	logger.Formatter = formatter
 
-	address = ":8082"
+	logger.WithFields(logrus.Fields{
+		"config": cfg,
+	}).Infoln("config file loaded")
+
+	port = cfg.Port
 	if cmdOpts.Port > 0 {
-		address = fmt.Sprintf(":%d", cmdOpts.Port)
+		port = cmdOpts.Port
 	}
+
+	address = fmt.Sprintf(":%d", port)
 	if lis, err = net.Listen("tcp", address); err != nil {
-		panic(err)
+		logger.WithFields(logrus.Fields{
+			"address": address,
+			"err":     err,
+		}).Panicln("could not created listener")
 	}
 
 	grpcServer = grpc.NewServer(grpcOpts...)
 	if srv, err = service.NewService(service.Config{
 		Logger: logger,
 	}); err != nil {
-		panic(err)
+		logger.WithFields(logrus.Fields{
+			"address": address,
+			"err":     err,
+		}).Panicln("could not created service")
 	}
 	pb.RegisterPaymentServiceServer(grpcServer, srv)
 
@@ -60,6 +109,9 @@ func main() {
 		"service_name": "payments",
 	}).Infoln("gRPC server started")
 	if err = grpcServer.Serve(lis); err != nil {
-		panic(err)
+		logger.WithFields(logrus.Fields{
+			"address": address,
+			"err":     err,
+		}).Panicln("could not start gRPC server")
 	}
 }
